0ef618619e25f598b864b7edab5e4525dcfeef4e
BTWitsK <BTWitsK@Gmail.com>
2022-01-05 00:02:52 -0600

    finished stage 1

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -1,7 +1,28 @@
 package numbers;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-//        write your code here
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a natural number: ");
+        int number = scanner.nextInt();
+
+        if (number < 1) {
+            System.out.println("This number is not natural!");
+        } else {
+            boolean doesEndIn7 = number % 10 == 7;
+            boolean isDivisible7 = number % 7 == 0;
+            boolean isBuzz = doesEndIn7 || isDivisible7;
+
+            String parity = number % 2 == 0 ? "This number is Even" : "This number is Odd";
+            String buzz = isBuzz ? "It is a Buzz number." : "It is not a Buzz number.";
+            String explanation = isBuzz ?
+                    doesEndIn7 && isDivisible7 ? "is divisible by 7 and ends with 7."
+                            : doesEndIn7 ? "ends with 7." : "is divisible by 7."
+                    : "is neither divisible by 7 nor does it end with 7";
+
+            System.out.printf("%s\n%s\nExplanation:\n%d %s", parity, buzz, number, explanation);
+        }
     }
 }
